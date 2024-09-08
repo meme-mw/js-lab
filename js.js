c9b5639571886fdfaeb37de51df8f35a5abe21fd
unknown <madaawadh1@gmail.com>
2024-09-08 12:13:51 +0300

    lab-js

---
+++ js.js
@@ -0,0 +1,49 @@
+//1st Q
+let height=10;
+let width=20;
+console.log("The area of the rectangle is: "+height*width );
+//2nd Q
+
+let C=41;
+
+let F=( C * 1.8) + 32;
+
+console.log(C +" °C is "+F+" °F" );
+C=(F - 32) * 5/9;
+console.log(F +" °F is "+C+" °C" );
+
+//3dd Q
+
+let hour=1;
+
+let sec=1200;
+
+console.log(1 +" Hour ="+1200+" Seconds" );
+
+//4th Q
+let firstVar="thingOne";
+let second="thingTwo";
+console.log(firstVar+second);
+//5th Q
+
+var bill=10.25 + 3.99 + 7.15;
+var tip=bill*.15;
+var total=bill+tip;
+console.log("total is "+total);
+
+//5th Q
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+
+var madLib="The Intro to JavaScript course is "+adjective1+  " James and Julia are so "+adjective2+" I cannot wait to work through the rest of this "+adjective3+" content!";
+
+console.log(madLib);
+
+var firstName="Mada";
+ var interest="sleeping";
+ var  hobby="read";
+
+ var awosomeMessage="Hi, my name is "+firstName+" I love "+interest+". In my spare time, I like to " +hobby+".";
+console.log(awosomeMessage);
